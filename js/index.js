0c5006f7a6ba6cd85626170ac6d8ce5c707595f3
Lazaro Pereira <lazaroairespereira@gmail.com>
2019-08-27 18:27:10 -0300

    first commit

---
+++ js/index.js
@@ -0,0 +1,33 @@
+$(document).ready(function () {
+    $(".owl-carousel").owlCarousel({
+        nav: false,
+        items: 1,
+        loop: true,
+        dots: true,
+        autoHeight: true
+    });
+});
+
+$("a.menu").click(function () {
+    var elemento = $(this).attr('href');
+    $('html, body').animate({
+        scrollTop: $(elemento).offset().top
+    }, 500);
+});
+
+setTimeout(() => {
+    console.log("oi");
+    $("#iframe").prop("hidden", false);
+}, 2000)
+
+let bool = true;
+$(document).scroll(() => {
+    if ((window.scrollY > $(".numeros").offset().top - window.innerHeight / 1.25) && bool) {
+        $('.counter1').animateNumber({ number: 455 }, 1200);
+        $('.counter2').animateNumber({ number: 1909100 }, 800);
+        $('.counter3').animateNumber({ number: 3599 }, 1000);
+        bool = false;
+    }
+});
+
+
