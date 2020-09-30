4478f21496ba238ec49cd2c2343877186a9f9b1e
mkconceicao <matheus.konradt.conceicao@gmail.com>
2020-09-30 10:20:30 -0300

    feat: add gtm

--- leitune-reguly/src/pages/_document.jsx
+++ leitune-reguly/src/pages/_document.jsx
@@ -44,18 +44,23 @@ export default class MyDocument extends Document {
           <meta property="og:type" content="website" />
           <link rel="canonical" href="https://leitunereguly.com" />
           {this.props.styleTags}
-          <script>
+          <script dangerouslySetInnerHTML={{
+            __html: `
             (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
               new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
               j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
               'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-K2BHSR8');
-          </script>
+            })(window,document,'script','dataLayer','GTM-K2BHSR8');`
+          }}
+          />
         </Head>
         <body id="top">
-        <Main />
-        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-K2BHSR8" height="0" width="0" style="display:none;visibility:hidden" /></noscript>
-        <NextScript />
+          <Main />
+          <noscript dangerouslySetInnerHTML={{
+            __html: '<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-K2BHSR8" height="0" width="0" style="display:none;visibility:hidden"></iframe>'
+          }}
+          />
+          <NextScript />
         </body>
       </Html>
     );
